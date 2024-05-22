f55b02e855e483251fdc0980f05247d164493937
Amarildo <you@example.com>
2024-05-22 15:16:55 +0200

    Add Main class to test Animal implementations

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal sparrow = new Sparrow();
+        Animal eagle = new Eagle();
+        Animal delfin = new Delfin();
+
+        dog.sleep();
+        dog.sound();
+        dog.eat();
+
+        sparrow.sleep();
+        sparrow.sound();
+        sparrow.eat();
+
+        eagle.sleep();
+        eagle.sound();
+        eagle.eat();
+
+        delfin.sleep();
+        delfin.sound();
+        delfin.eat();
+
+
+    }
+}
