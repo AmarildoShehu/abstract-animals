d7e70dd5b0914fe0d20a12d595e370c7f3984260
Amarildo <you@example.com>
2024-05-22 15:41:21 +0200

    Add Util class with makeFly and makeSwim methods

---
+++ src/Util.java
@@ -0,0 +1,9 @@
+public class Util {
+    public static void makeFly(Flying animal) {
+        animal.fly();
+    }
+
+    public static void makeSwim(Swimmer animal) {
+        animal.swim();
+    }
+}
