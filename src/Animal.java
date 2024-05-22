6f34409219fd1b625a4d94029d233c02f9630b59
Amarildo <you@example.com>
2024-05-22 14:48:11 +0200

    create abstract class Animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,9 @@
-public class Animal {
+public abstract class Animal {
+    public void sleep() {
+        System.out.println("Zzzzz");
+    }
+
+    public abstract void sound ();
+
+    public abstract void eat();
 }
